ee5f536c09a84b739582eaab9ae938f45a1753b7
amanbadyal7214 <amanbadyal94@gmail.com>
2026-03-26 10:20:29 +0530

    df

--- Models/Product.js
+++ Models/Product.js
@@ -4,6 +4,7 @@ const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   category: { type: String, required: true },
+  stock: { type: Number, default: 0 },
   img: { type: String }, // Primary image web path
   imgBase64: { type: String }, // Primary image base64
   images: [{
