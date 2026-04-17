2d28ede2641ea0cd3f8dbff9f67bfdc0de61243f
amanbadyal7214 <amanbadyal94@gmail.com>
2026-04-17 14:59:31 +0530

    vh

--- Controllers/categoryController.js
+++ Controllers/categoryController.js
@@ -1,4 +1,10 @@
 const Category = require('../Models/Category');
+const Flavor = require('../Models/Flavor');
+const Type = require('../Models/Type');
+const Shape = require('../Models/Shape');
+const Occasion = require('../Models/Occasion');
+const Weight = require('../Models/Weight');
+const Theme = require('../Models/Theme');
 
 exports.listCategories = async (req, res, next) => {
   try {
@@ -42,10 +48,37 @@ exports.updateCategory = async (req, res, next) => {
 
 exports.deleteCategory = async (req, res, next) => {
   try {
-    const removed = await Category.findByIdAndDelete(req.params.id);
+    const categoryId = req.params.id;
+
+    // Check if category is used in any of the sub-types
+    const [flavors, types, shapes, occasions, weights, themes] = await Promise.all([
+      Flavor.findOne({ categories: categoryId }),
+      Type.findOne({ categories: categoryId }),
+      Shape.findOne({ categories: categoryId }),
+      Occasion.findOne({ categories: categoryId }),
+      Weight.findOne({ categories: categoryId }),
+      Theme.findOne({ categories: categoryId }),
+    ]);
+
+    const linkedItems = [];
+    if (flavors) linkedItems.push('Flavor');
+    if (types) linkedItems.push('Type');
+    if (shapes) linkedItems.push('Shape');
+    if (occasions) linkedItems.push('Occasion');
+    if (weights) linkedItems.push('Weight');
+    if (themes) linkedItems.push('Theme');
+
+    if (linkedItems.length > 0) {
+      return res.status(400).json({ 
+        error: `Cannot delete category. It is linked to the following sub-types: ${linkedItems.join(', ')}. Please delete or update those first.` 
+      });
+    }
+
+    const removed = await Category.findByIdAndDelete(categoryId);
     if (!removed) return res.status(404).json({ error: 'Category not found' });
     res.json({ data: { success: true } });
   } catch (err) {
     next(err);
   }
 };
+
